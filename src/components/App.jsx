b0d6571d3f055ffd5cd60dfcb1956d25abae761a
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-09-15 12:07:11 +0300

    Suspense moved to SharedLayout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { lasy, Suspense } from 'react';
+import { lasy } from 'react';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { Cast } from 'components/Cast/Cast';
 import { Reviews } from 'components/Reviews/Reviews';
@@ -10,7 +10,6 @@ const MovieDetails = lasy(() => import('pages/MovieDetails/MovieDetails.jsx'));
 const NotFound = lasy(() => import('pages/NotFound/NotFound.jsx'));
 
 export const App = () => {
-  <Suspense fallback={<div>Loading...</div>}></Suspense>;
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container, Header, StyledLink } from './SharedLayout.styled';
 
@@ -18,7 +19,9 @@ export const SharedLayout = () => {
         </nav>
       </Header>
       <main>
-        <Outlet />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </main>
     </Container>
   );
