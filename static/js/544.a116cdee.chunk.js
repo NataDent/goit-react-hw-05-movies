23cb6df1b554154276d02d263aba7435c03e3429
NataDent <125708241+NataDent@users.noreply.github.com>
2023-09-15 10:46:21 +0000

    Deploying to gh-pages from @ NataDent/goit-react-hw-05-movies@5f116f820d1cc9d5fbe198b3e8600f2e5a2ff479 🚀

---
+++ static/js/544.a116cdee.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,r){r.r(t),r.d(t,{Home:function(){return c}});var n=r(184),c=function(){return(0,n.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=544.a116cdee.chunk.js.map
