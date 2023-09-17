2adc35b71745d4a99d4a850bb3d3652bef7cb5c5
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-09-18 01:12:36 +0300

    Changed path in MoviesGallery

--- src/components/MoviesGallery/MoviesGallery.jsx
+++ src/components/MoviesGallery/MoviesGallery.jsx
@@ -1,13 +1,18 @@
 import { Loader } from "utils/Loader";
 import { GalleryItemStyled, MovieTitle, MoviesGalleryStyled } from "./MoviesGallery.styled";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { ToastContainer, toast } from 'react-toastify';
+import { Link, useLocation } from "react-router-dom";
 
 
 
+const BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 export const MoviesGallery = ({movies, loadMore}) => {
+  const location = useLocation();
   return (
     <>
+    <ToastContainer autoclose={5000}/>
           {movies.length > 0 && (
         <InfiniteScroll
           dataLength={movies.length} //This is important field to render the next data
@@ -21,17 +26,23 @@ export const MoviesGallery = ({movies, loadMore}) => {
           }
         >
           <MoviesGalleryStyled>
-            {movies.map(({id, poster_path, title}) => (
-              <GalleryItemStyled key={id}>
+            {movies.map(({id,  title,  backdrop_path}) => {
+            const image = backdrop_path ? `${BASE_URL}${backdrop_path}` : toast.error('Oops! There was an error!');
+            return(
+               <GalleryItemStyled key={id}>
+
+                <Link to={`/movies/${id}`} state={{ from: location }}>
                 <img
-                  src={`https://image.tmdb.org/t/p/w300${poster_path}`}
+                  src={image}
                   alt={title}
                   width="300px"
                   height="450px"
                 />
                 <MovieTitle>{title}</MovieTitle>
-              </GalleryItemStyled>
-            ))}
+                </Link>
+
+              </GalleryItemStyled>)
+})}
           </MoviesGalleryStyled>
         </InfiniteScroll>
       )}
