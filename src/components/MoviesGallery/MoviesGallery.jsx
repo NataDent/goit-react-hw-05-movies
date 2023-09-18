d229d40867cabc9c9f1c94e7cb52843790f69f95
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-09-18 11:31:03 +0300

    InfiniteScroll moved to Homepage

--- src/components/MoviesGallery/MoviesGallery.jsx
+++ src/components/MoviesGallery/MoviesGallery.jsx
@@ -1,33 +1,23 @@
-import { Loader } from "utils/Loader";
+
 import { GalleryItemStyled, MovieTitle, MoviesGalleryStyled } from "./MoviesGallery.styled";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { ToastContainer, toast } from 'react-toastify';
+
 import { Link, useLocation } from "react-router-dom";
+import NotFound from "pages/NotFound/NotFound";
 
 
 
 const BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
-export const MoviesGallery = ({movies, loadMore}) => {
+export const MoviesGallery = ({movies}) => {
   const location = useLocation();
   return (
     <>
-    <ToastContainer autoclose={5000}/>
           {movies.length > 0 && (
-        <InfiniteScroll
-          dataLength={movies.length} //This is important field to render the next data
-          next={loadMore}
-          hasMore={true}
-          loader={<Loader key={0} loading={true} />}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b> You've already seen it all</b>
-            </p>
-          }
-        >
+        
           <MoviesGalleryStyled>
             {movies.map(({id,  title,  backdrop_path}) => {
-            const image = backdrop_path ? `${BASE_URL}${backdrop_path}` : toast.error('Oops! There was an error!');
+            const image = backdrop_path ? `${BASE_URL}${backdrop_path}` : <NotFound />
+
             return(
                <GalleryItemStyled key={id}>
 
@@ -44,7 +34,7 @@ export const MoviesGallery = ({movies, loadMore}) => {
               </GalleryItemStyled>)
 })}
           </MoviesGalleryStyled>
-        </InfiniteScroll>
+      
       )}
     </>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,28 +2,44 @@ import { MoviesGallery } from 'components/MoviesGallery/MoviesGallery';
 import { useState, useEffect } from 'react';
 import { getAllTrandingMoviesToday, getMovieDetailsById } from 'utils/api';
 import { Container, SectionStyled, SectionTitle } from './Home.styled';
+import { Loader } from "utils/Loader";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const[page,setPage]=useState(1);
 
   useEffect(() => {
     const getMovies = async () => {
       try {
         const { results } = await getAllTrandingMoviesToday();
         setMovies(results);
-        
+        setPage(prevPage=> prevPage+1);
       } catch (error) {
         console.log(error.message);
       }
     };
-    getMovies();
-  }, []);
+    getMovies(page);
+  }, [page]);
 
   return (
     <SectionStyled>
       <Container>
         <SectionTitle>Trending today</SectionTitle>
+        <InfiniteScroll
+          dataLength={movies.length} //This is important field to render the next data
+          next={getAllTrandingMoviesToday}
+          hasMore={true}
+          loader={<Loader key={0} loading={true} />}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b> You've already seen it all</b>
+            </p>
+          }
+        >
         <MoviesGallery movies={movies} onClick={getMovieDetailsById} />
+        </InfiniteScroll>
       </Container>
     </SectionStyled>
   );
